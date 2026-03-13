57474bd7af11964a37598b69b3bf1fdc270d87b1
Umoh Samuel <samuelwisdom360@gmail.com>
2026-03-13 18:39:32 +0100

    setup glfw as window manager and initialize opengl

---
+++ HelloOpenGL/Main.cpp
@@ -0,0 +1,54 @@
+#include "glad/glad.h" // glad manages function pointers for OpenGL
+#include "GLFW/glfw3.h"
+
+#include <iostream>
+
+// handle window resize
+void frame_buffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+}
+
+int main()
+{
+    // initialize glfw
+	glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    const int windowHeight = 800;
+    const int windowWidth = 600;
+
+    // create a window object
+    GLFWwindow* window = glfwCreateWindow(windowHeight, windowWidth, "HelloOpenGL", NULL, NULL);
+    if (window == NULL)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+    glfwSetFramebufferSizeCallback(window, frame_buffer_size_callback);
+
+    // inittialize glad before calling any opengl functions
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+
+    // configure the size of the rendering window for opengl
+    glViewport(0, 0, windowWidth, windowHeight);
+
+    // render loop
+    while (!glfwWindowShouldClose(window))
+    {
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
+}
